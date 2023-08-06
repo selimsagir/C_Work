458b7672d1e0a2fa18fdc8b4c6a14331d5fcbd28
selimsagir <selimsagir61@gmail.com>
2023-08-06 02:38:20 +0200

    fft and dft implementation with execution time calculation in makefile

--- fft/fft.c
+++ fft/fft.c
@@ -5,6 +5,49 @@
 
 float pi;
 
+void printSignal(float *arr, size_t size){
+        // Print the generated input signal values
+    for (size_t i = 0; i < size; i++)
+    {   // print signal
+         printf("%f\n", arr[i]);
+    }
+}
+
+
+void dft(float in[], float complex out[], size_t n){
+
+    for (size_t f = 0; f < n; f++){
+        out[f] = 0;
+        for (size_t i = 0; i < n; i++){
+            float t = (float)i/n;
+            out[f] += in[i]*cexp(2*I*pi*f*t);
+        }
+        
+    }
+}
+
+void fft(float in[], size_t stride, float complex out[], size_t n){
+
+    if(n == 1){
+        out[0] = in[0];
+        return;
+    } 
+
+    fft(in,           stride*2, out,       n/2); 
+    fft(in + stride , stride*2, out + n/2, n/2);
+
+    for (size_t k = 0; k < n/2; ++k)
+    {
+        float t = (float)k/n;
+        float complex v = cexp(-2*I*pi*t)*out[k + n/2];
+        float complex e = out[k];
+        out[k] = e + v;
+        out[k + n/2] = e - v;
+    }
+    
+
+}
+
 int main()
 {
     // Calculate the value of pi and store it in the global variable 'pi'
@@ -13,7 +56,7 @@ int main()
     // Multiplying by 4 gives the value of pi and stores it in 'pi' for later use.
 
     // Set the number of samples 'n' to 8
-    size_t n = 8;
+    size_t n = 64*4  ;
 
     // Declare two arrays to hold the input and output signals, both of size 'n'
     float in[n];
@@ -25,38 +68,17 @@ int main()
         // Calculate the time value 't' for the current sample
         float t = (float)i / n;
         // Generate a complex waveform by summing two sine waves of different frequencies:
-        in[i] = sinf(2 * pi * t * 1) + sinf(2 * pi * t * 3);
+        in[i] = cosf(2 * pi * t * 1) + sinf(2 * pi * t * 2);
         // The first sine wave has a frequency of 1 Hz, and the second has a frequency of 3 Hz.
     }
 
-    // Print the generated input signal values
-    for (size_t i = 0; i < n; i++)
-    {   // print signal
-        // printf("%f\n", in[i]);
-    }
-
-    // Perform the FFT algorithm to transform the input signal to the frequency domain
-    for (size_t f = 0; f < n; f++)
-    {
-        // Initialize the output frequency bin 'out[f]' to 0
-        out[f] = 0;
-
-        // Loop over all input samples to compute the FFT output at frequency bin 'f'
-        for (size_t i = 0; i < n; i++)
-        {
-            // Calculate the time value 't' for the current input sample
-            float t = (float)i / n;
-            // Compute the contribution of the current input sample to the output bin 'out[f]'
-            // out[f] += in[i] * sinf(2 * pi * f * t);
-            out[f] += in[i]*cexp(2*I*pi*f*t);
-            // The multiplication of 'in[i]' with 'sinf(2*pi*f*t)' represents the complex exponential term in the FFT formula.
-        }
-    }
-
+    // dft(in, out, n);
+    fft(in,1, out, n);
+    
     // Print the FFT results for each frequency bin
     for (size_t f = 0; f < n; f++)
     {
-        printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
+        printf("%02zu: %.2f, %.2fi\n", f, creal(out[f]), cimag(out[f]));
     }
 
     return 0;
